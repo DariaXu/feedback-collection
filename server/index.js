de39819b3de7bed0cf5c7f088410c480b77622f7
Daria <31807826+DariaXu@users.noreply.github.com>
2022-09-03 19:44:12 -0400

    Before deploy

---
+++ server/index.js
@@ -0,0 +1,14 @@
+// commonjs module
+const express = require('express');
+// application(running express app)
+const app = express();
+
+// res=>request, res => responds
+app.get('/', (req, res) => {
+    // send json data
+    res.send({ hi : 'there' });
+});
+
+// telling node js to listen to port 8000
+const PORT = process.env.PORT || 8000
+app.listen(PORT);
