6e52f1b835aede29b45ed657b49ab06c50a9fbe6
Daria <31807826+DariaXu@users.noreply.github.com>
2022-09-06 18:34:02 -0400

    fixed key

--- config/keys.js
+++
@@ -1,7 +0,0 @@
-// check if in production
-if (process.env.NODE_ENV == 'production'){
-    // run in heroku
-    module.exports = require('./prod');
-}else{
-    module.exports = require('./dev');
-}
--- middlewares/requireLogin.js
+++
@@ -1,8 +0,0 @@
-// next called when middleware finish running, and pass to the next middleware
-module.exports = (req, res, next) => {
-    if (!req.user) {
-        return res.status(401).send({error: 'You must log in!'});
-    }
-
-    next();
-};
